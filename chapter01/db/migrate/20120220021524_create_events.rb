fe9cc2fa30ca03f8aa6739b87283d84b1125e5e3
Eduardo Maia <eduvimaia@gmail.com>
2012-02-20 00:17:03 -0200

    criacao da entidade Events

---
+++ chapter01/db/migrate/20120220021524_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :artist
+      t.text :description
+      t.decimal :price_low
+      t.decimal :price_high
+      t.date :event_date
+
+      t.timestamps
+    end
+  end
+end
--- chapter01/db/schema.rb
+++ chapter01/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120220015704) do
+ActiveRecord::Schema.define(:version => 20120220021524) do
+
+  create_table "events", :force => true do |t|
+    t.string   "artist"
+    t.text     "description"
+    t.decimal  "price_low"
+    t.decimal  "price_high"
+    t.date     "event_date"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "tickets", :force => true do |t|
     t.string   "name"
