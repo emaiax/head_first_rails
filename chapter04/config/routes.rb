00b60f0165e515e83eb7750c52d54963ae54058b
Eduardo Maia <eduvimaia@gmail.com>
2012-02-21 20:46:20 -0200

    chapter 4 - configurando routes para o controller client_workouts

--- chapter04/config/routes.rb
+++ chapter04/config/routes.rb
@@ -1,4 +1,6 @@
 Chapter04::Application.routes.draw do
+  resources :client_workouts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,8 +50,8 @@ Chapter04::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  root :to => 'client_workouts#index'
+	
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
