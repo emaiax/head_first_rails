12a48a2d4b105eabda251144567689ffb35bd8bc
Eduardo Maia <eduvimaia@gmail.com>
2012-02-22 19:03:39 -0200

    chapter 5 - movendo app client_workouts para subdiretório

--- chapter05/app/models/client_workout.rb
+++
@@ -1,7 +0,0 @@
-class ClientWorkout < ActiveRecord::Base
-	validates :client_name, :presence => true
-	validates :trainer, :presence => true
-
-	validates :paid_amount, :numericality => { :greater_than => 0 }
-	validates :duration_mins, :numericality => { :greater_than => 0 }
-end
--- chapter05/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Chapter04::Application.config.secret_token = '49ce92c41b27379b0041eab7ee6e04f9eaee05d093839c5118b7d5fbe5712ae72499637a616ac727e435d780b6bb7af55b8d100444787014a985c2336b59134b'
