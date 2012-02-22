d1fba884d6d11577e92dcaf0a138d3385933fa01
Eduardo Maia <eduvimaia@gmail.com>
2012-02-22 19:12:11 -0200

    chapter 5 - inclusão de validatons no model ad

--- chapter05/mebay/app/models/ad.rb
+++ chapter05/mebay/app/models/ad.rb
@@ -2,5 +2,5 @@ class Ad < ActiveRecord::Base
 	validates :name, :presence => true
 	validates :description, :presence => true
 	validates :seller_id, :presence => true
-	validates :price, :presence => true
+	validates :price, :numericality => { :greater_than => 0 }
 end
